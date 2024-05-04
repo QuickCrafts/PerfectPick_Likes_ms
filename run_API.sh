e7ebdb28db6e7e6200912d3710ce6a4564657848
Paula Guzmán <pauladaguz@gmail.com>
2024-05-04 11:46:03 -0500

    Advance Automatic

--- run_API.sh
+++ run_API.sh
@@ -7,5 +7,15 @@
 # echo "docker run..."
 # docker run -p 3000:3000 go-containerized:latest
 
-docker-compose build
+echo "Delete existing Likes container"
+
+docker stop neo4j PerfectPick_Likes_ms
+docker rm neo4j PerfectPick_Likes_ms
+
+echo "Delete existing Likes image"
+
+docker rmi neo4j PerfectPick_Likes_ms
+
+echo "running docker..."
+
 docker-compose up
