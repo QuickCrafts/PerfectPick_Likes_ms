5f8da81b6de48384e0418c4127706618c31bb5d1
Paula Guzmán <pauladaguz@gmail.com>
2024-05-04 12:50:54 -0500

    Advance Automatic

--- run_API.sh
+++ run_API.sh
@@ -1,12 +1,3 @@
-
-# echo "docker build..."
-# docker build . -t go-containerized:latest
-
-# echo ""
-# echo ""
-# echo "docker run..."
-# docker run -p 3000:3000 go-containerized:latest
-
 echo "Delete existing Likes container"
 
 docker stop neo4j PerfectPick_Likes_ms
